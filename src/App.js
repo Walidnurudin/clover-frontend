1a83e03f2c6d3965adda58e3e9a9fedaa30310b0
Walidnurudin <walidnurudin47@gmail.com>
2021-11-05 23:51:23 +0700

    downgrade react-router-dom 6.0.0 to 5.2.0

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// PAGES
+import LandingPage from "./pages/LandingPage";
+
 function App() {
-  return <div>tes</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/landing-page" exact component={LandingPage} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;

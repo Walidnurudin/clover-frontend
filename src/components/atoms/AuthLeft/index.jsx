332552e86c49c60359a85747ae074b6c1919f26c
AhmadZaky19 <a.zaky32@gmail.com>
2021-11-06 18:31:56 +0700

    add AuthLeft component

---
+++ src/components/atoms/AuthLeft/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Image } from "react-bootstrap";
+import "./index.css";
+import { FormImage, MaskFormImage } from "../../../assets/images";
+
+const AuthLeft = () => {
+  return (
+    <>
+      <div className="row__overlay">
+        <Image src={MaskFormImage} alt="App" className="row__overlay--mask" />
+        <span className="row__overlay--quote">
+          <h1>
+            Temukan developer <br />
+            berbakat & terbaik <br />
+            di berbagai bidang <br />
+            keahlian
+          </h1>
+        </span>
+      </div>
+      <Image src={FormImage} alt="Landing page" className="row__image" />{" "}
+    </>
+  );
+};
+
+export default AuthLeft;

528c882fa577ee24f6df9114eda1045e56c02c27
riyaraa <rinosatyaputra.id@gmail.com>
2021-11-06 21:53:29 +0700

    update: Homepage Slicing

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // PAGES
 import LandingPage from "./pages/LandingPage";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={LandingPage} />
+        <Route exact path="/home" component={Home} />
       </Switch>
     </Router>
   );
--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,5 +3,19 @@ import LandingPage2 from "./LandingPage2.png";
 import LandingPage3 from "./LandingPage3.png";
 import TickPurple from "./tick-purple.png";
 import TickYellow from "./tick-yellow.png";
+import Profile from "./Profile.png";
+import Polygon from "./ArrowUp.png";
+import Union from "./Union.svg";
+import Bell from "./Bell.svg";
 
-export { LandingPage1, LandingPage2, LandingPage3, TickPurple, TickYellow };
+export {
+  LandingPage1,
+  LandingPage2,
+  LandingPage3,
+  TickPurple,
+  TickYellow,
+  Profile,
+  Polygon,
+  Union,
+  Bell
+};

943ba47c2a284f661ae825abf843a89c6f31bc4f
AhmadZaky19 <a.zaky32@gmail.com>
2021-11-06 23:59:01 +0700

    add login page (slicing)

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // PAGES
 import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={LandingPage} />
+        <Route path="/login" exact component={Login} />
       </Switch>
     </Router>
   );
--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,5 +3,15 @@ import LandingPage2 from "./LandingPage2.png";
 import LandingPage3 from "./LandingPage3.png";
 import TickPurple from "./tick-purple.png";
 import TickYellow from "./tick-yellow.png";
+import FormImage from "./FormImage.png";
+import MaskFormImage from "./MaskFormImage.png";
 
-export { LandingPage1, LandingPage2, LandingPage3, TickPurple, TickYellow };
+export {
+  LandingPage1,
+  LandingPage2,
+  LandingPage3,
+  TickPurple,
+  TickYellow,
+  FormImage,
+  MaskFormImage
+};

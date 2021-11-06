f24f87a170ef09c4a7d196847963d5ee02c80ad7
AhmadZaky19 <a.zaky32@gmail.com>
2021-11-07 00:37:41 +0700

    add register for workers page (slicing)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // PAGES
 import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
+import RegisterWorkers from "./pages/RegisterWorkers";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={LandingPage} />
         <Route path="/login" exact component={Login} />
+        <Route path="/register-workers" exact component={RegisterWorkers} />
       </Switch>
     </Router>
   );
---
+++ src/pages/RegisterWorkers/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "./index.css";
+import AuthLeft from "../../components/atoms/AuthLeft";
+import FormRegisterWorkers from "../../components/molecules/Forms/RegisterWorkers";
+
+const RegisterWorkers = () => {
+  return (
+    <>
+      <Container fluid className="window">
+        <Row>
+          <Col md={6} className="column1">
+            <AuthLeft />
+          </Col>
+          <Col md={6} className="column2">
+            <FormRegisterWorkers />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default RegisterWorkers;

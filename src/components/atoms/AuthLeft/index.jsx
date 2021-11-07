18d12bc244a9e8de1e17f542c3fb104ec6ca83e0
AhmadZaky19 <a.zaky32@gmail.com>
2021-11-08 03:33:04 +0700

    add logo

--- src/components/atoms/AuthLeft/index.jsx
+++ src/components/atoms/AuthLeft/index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Image } from "react-bootstrap";
 import "./index.css";
-import { FormImage, MaskFormImage } from "../../../assets/images";
+import { FormImage, MaskFormImage, LogoWhite } from "../../../assets/images";
 
 const AuthLeft = () => {
   return (
     <>
       <div className="row__overlay">
         <Image src={MaskFormImage} alt="App" className="row__overlay--mask" />
+        <span className="row__overlay--quote">
+          <Image src={LogoWhite} className="row__overlay--logo" />
+        </span>
         <span className="row__overlay--quote">
           <h1>
             Temukan developer <br />
